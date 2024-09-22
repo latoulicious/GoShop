3b714f07cdd6b47aa85b319a43f124a63677ca81
latoulicious <45010096+latoulicious@users.noreply.github.com>
2024-09-22 19:58:25 +0700

    fix: remove broken docker component (#3)
    * Feat: implement air
    
    * Fix: remove broken docker component

--- cmd/main.go
+++ cmd/main.go
@@ -1,130 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"time"
+import "github.com/gofiber/fiber/v2"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	jwtware "github.com/gofiber/jwt/v3"
-	"github.com/golang-jwt/jwt/v5"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-
-	_ "github.com/latoulicious/goshop/docs" // This imports the Swagger docs
-
-	httpSwagger "github.com/swaggo/http-swagger" // The official swaggo package
-)
-
-// Database connection
-var DB *gorm.DB
-
-type User struct {
-	ID       uint   `json:"id" gorm:"primaryKey"`
-	Username string `json:"username" gorm:"unique"`
-	Password string `json:"password"`
-}
-
-// InitDatabase sets up the database connection
-func InitDatabase() {
-	dsn := "host=postgres user=postgres password=secret dbname=testdb port=5432 sslmode=disable"
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Failed to connect to database!", err)
-	}
-	log.Println("Database connected!")
-	DB.AutoMigrate(&User{})
-}
-
-// Generate JWT token
-func GenerateJWT(username string) (string, error) {
-	claims := jwt.MapClaims{
-		"username": username,
-		"exp":      time.Now().Add(time.Hour * 72).Unix(),
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-}
-
-// Login endpoint
-func Login(c *fiber.Ctx) error {
-	type LoginInput struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
-	}
-	input := new(LoginInput)
-	if err := c.BodyParser(input); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request"})
-	}
-
-	var user User
-	if err := DB.Where("username = ? AND password = ?", input.Username, input.Password).First(&user).Error; err != nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Invalid credentials"})
-	}
-
-	token, err := GenerateJWT(user.Username)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Could not log in"})
-	}
-
-	return c.JSON(fiber.Map{"token": token})
-}
-
-// Authenticated route
-func Protected(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"message": "This is a protected route"})
-}
-
-// Setup routes
-func SetupRoutes(app *fiber.App) {
-	app.Get("/swagger/*", httpSwagger.WrapHandler) // Official swaggo Swagger route
-	app.Post("/login", Login)
-
-	// JWT middleware for protecting routes
-	app.Use("/protected", jwtware.New(jwtware.Config{
-		SigningKey: []byte(os.Getenv("JWT_SECRET")),
-	}))
-
-	app.Get("/protected", Protected)
-}
-
-// @title Go Fiber Example API
-// @version 1.0
-// @description This is a simple API for demonstration purposes.
-// @termsOfService http://swagger.io/terms/
-
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @host localhost:3000
-// @BasePath /
 func main() {
-	// Initialize Fiber
 	app := fiber.New()
 
-	// Middleware
-	app.Use(logger.New())
-	app.Use(cors.New())
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
-	// Connect to the database
-	InitDatabase()
-
-	// Set up routes
-	SetupRoutes(app)
-
-	// Start server
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-
-	log.Fatal(app.Listen(fmt.Sprintf(":%s", port)))
+	app.Listen(":3000")
 }
