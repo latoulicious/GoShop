6a2d847c6f3fdd4f4251d9223ba2a3f87792196a
arsyds <arsyds@proton.me>
2024-09-22 14:22:20 +0700

    Fix: move main.go

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	jwtware "github.com/gofiber/jwt/v3"
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
