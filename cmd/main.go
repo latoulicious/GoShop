8687a338f78a3913d7cd4c6ef3385d992315f1f8
arsyds <arsyds@proton.me>
2024-09-23 21:12:49 +0700

    Docs: add .env example

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,82 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"github.com/latoulicious/GoShop/config"
+	"github.com/latoulicious/GoShop/database"
+	"github.com/latoulicious/GoShop/utils"
+)
+
+// Declare dsn at the package level
+var DSN string
+
+// main is the entry point for the application. It loads configuration, initializes dependencies like the database, sets up routes and middleware, and starts the HTTP server.
 func main() {
+	// Load configuration from .env file
+	config.LoadConfig()
+
+	if os.Getenv("ENVIRONMENT") == "development" {
+
+		log.Println("Checking if JWT secret is set")
+
+		// If JWT_SECRET is not set, generate a new JWT secret key
+		if config.JwtSecret == "" {
+			log.Println("JWT secret is empty, generating a new one")
+			// Generate the JWT secret key
+			secretKey, err := utils.GenerateJWTSecretKey()
+			if err != nil {
+				log.Fatal("Error generating JWT secret key:", err)
+			}
+
+			// Print or log the generated JWT secret key
+			log.Println("Generated JWT Secret Key:", secretKey)
+
+			// Set the generated JWT secret key in the configuration
+			config.JwtSecret = secretKey
+		}
+
+	}
+
+	// Initialize Fiber app
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	// Setup CORS
+	config.SetupCors(app)
+
+	// Logging Request ID
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		// For more options, see the Config section
+		Format: "${pid} ${locals:requestid} ${status} - ${method} ${path}​\n",
+	}))
+
+	// Connect to the database
+	err := database.ConnectDB()
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+
+	// Setup routes
+	// router.SetupRoutes(app)
+
+	// Start the server and handle errors
+	err = app.Listen(":3000")
+	if err != nil {
+		log.Fatal("Failed to start the server:", err)
+	}
+
+	// Graceful shutdown
+	err = app.Shutdown()
+	if err != nil {
+		log.Println("Graceful shutdown failed:", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.23.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/google/uuid v1.5.0
+	github.com/joho/godotenv v1.5.1
 	github.com/swaggo/swag v1.8.1
+	gorm.io/driver/postgres v1.5.9
+	gorm.io/gorm v1.25.12
 )
 
 require (
@@ -14,22 +18,27 @@ require (
 	github.com/go-openapi/jsonreference v0.20.0 // indirect
 	github.com/go-openapi/spec v0.20.6 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
-	github.com/google/uuid v1.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
-	github.com/kr/pretty v0.3.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
